0fad9797c91548457dfaa4a1515cb824f5b740da
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-21 15:48:27 +0200

    start

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+
+
+// //chiedere all'utente quanti km deve fare
+// let kmTravel = parseInt( ('how many km do you have to travel?') ) 
+// kmTravel = Math.abs(kmTravel)
+
+//chiedere all'utente quanti anni ha 
+// let age = parseInt( prompt('how old are you?') )
+
+// //controllare validità dei dati
+// if(isNaN(age)) {
+//     age = 50
+// }
+
+// if(isNaN(kmTravel)) {
+//     alert('distance not valid')
+// }
+
+// //costo al km
+// const oneKmPrice = 0.21
+
+// //calcolo il prezzo in base ai km
+// let ticketPrice = (kmTravel * oneKmPrice)
+
+// console.log(kmTravel , age , ticketPrice)
+
+
+// //se l'età è minore di 18
+// if ( age < 18 ) {
+//     ticketPrice = ticketPrice * 0.8
+//     console.log(ticketPrice)
+// }
+
+// //se l'età è maggiore di 65
+// if ( age > 65 ) {
+//     ticketPrice = ticketPrice * 0.6
+//     console.log(ticketPrice)
+// }
